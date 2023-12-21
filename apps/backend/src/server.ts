7f92b74161baf0706b2b090f1fca582261d17ffb
Pavan Chindukuri <chindukuripavan@gmail.com>
2023-12-21 11:36:03 +0530

    Remove unused code and dependencies

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,37 +1,17 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import session from 'express-session';
 import * as path from 'path';
 import pino from 'pino-http';
 import authRouter from 'src/api/routes/auth.routes';
-import { createClient } from 'redis';
-import RedisStore from 'connect-redis';
 
 const app = express();
 
-const redisClient = createClient({
-  url: ' redis://localhost:6379',
-});
-redisClient.connect().catch(console.error);
-const redisStore = new RedisStore({
-  client: redisClient,
-  prefix: 'session-store:',
-});
 app.use(
   pino({
     level: 'debug',
   })
 );
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: true },
-    store: redisStore,
-  })
-);
 app.use(cookieParser());
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
